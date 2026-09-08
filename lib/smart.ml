dacb9f3948cce1498b6dd4ab8eed009280bab0ce
Calascibetta Romain <romain.calascibetta@gmail.com>
2026-09-08 15:34:55 +0200

    Implement the version=1 of the Smart protocol

--- lib/smart.ml
+++ lib/smart.ml
@@ -3,12 +3,20 @@ let ( let* ) = Protocol.bind
 
 module Log = (val Logs.src_log src : Logs.LOG)
 
-type error = [ Protocol.error | `No_branch | `Invalid_version of string ]
+type error =
+  [ Protocol.error
+  | `No_branch
+  | `Invalid_version of string
+  | `No_side_band
+  | `Err of string ]
 
 let pp_error ppf = function
   | #Protocol.error as err -> Protocol.pp_error ppf err
   | `No_branch -> Fmt.string ppf "No branch available"
   | `Invalid_version v -> Fmt.pf ppf "Invalid Smart version: %S" v
+  | `No_side_band ->
+      Fmt.string ppf "The remote does not support the side-band capability"
+  | `Err msg -> Fmt.pf ppf "Remote error: %s" msg
 
 type refs = {
   refs : (string * string) list (* refname, oid (hex) *);
@@ -17,6 +25,77 @@ type refs = {
   head : Carton.Uid.t; (* oid (hex) *)
   head_symref : string option (* refs/heads/main *);
 }
+
+type advertisement =
+  | V1 of { refs : refs; capabilities : string list }
+  | V2 of { capabilities : string list }
+
+let attribute ~prefix attrs =
+  let fn attr =
+    if String.starts_with ~prefix attr
+    then
+      let off = String.length prefix
+      and len = String.length attr - String.length prefix in
+      Some (String.sub attr off len)
+    else None in
+  List.find_map fn attrs
+
+let err_of_pkt pkt = attribute ~prefix:"ERR " [ pkt ]
+let uid_of_hex hex = Carton.Uid.unsafe_of_string (Ohex.decode hex)
+
+let head_of_refs refs head_symref =
+  match (List.assoc_opt "HEAD" refs, head_symref) with
+  | Some head, _ when head <> "unborn" -> Protocol.return (uid_of_hex head)
+  | _, Some symref ->
+      begin match List.assoc_opt symref refs with
+      | Some head -> Protocol.return (uid_of_hex head)
+      | None -> Protocol.error `No_branch
+      end
+  | _ -> Protocol.error `No_branch
+
+let ref_of_line line =
+  match String.split_on_char ' ' (String.trim line) with
+  | [ oid; name ] -> Some (name, oid)
+  | _ -> None
+
+let advertisement_v1 first ctx =
+  let first, capabilities =
+    match String.index_opt first '\000' with
+    | Some idx ->
+        ( String.sub first 0 idx,
+          String.sub first (idx + 1) (String.length first - idx - 1) )
+    | None -> (first, "") in
+  let capabilities =
+    List.filter (( <> ) "")
+      (String.split_on_char ' ' (String.trim capabilities)) in
+  let head_symref = attribute ~prefix:"symref=HEAD:" capabilities in
+  let rec go acc ctx =
+    let* pkt = Protocol.decode_pkt ctx in
+    match String.trim pkt with
+    | "" -> Protocol.return (List.rev acc)
+    | line ->
+        begin match ref_of_line line with
+        | Some value -> go (value :: acc) ctx
+        | None -> Protocol.error `Invalid_pkt_line
+        end in
+  match (err_of_pkt first, ref_of_line first) with
+  | Some msg, _ -> Protocol.error (`Err msg)
+  | None, None -> Protocol.error `Invalid_pkt_line
+  | None, Some value ->
+      let* advertised = go [ value ] ctx in
+      let fn (refs, peeled) (name, oid) =
+        if name = "capabilities^{}"
+        then (refs, peeled)
+        else if String.ends_with ~suffix:"^{}" name
+        then
+          let name = String.sub name 0 (String.length name - 3) in
+          (refs, (name, oid) :: peeled)
+        else ((name, oid) :: refs, peeled) in
+      let refs, peeled = List.fold_left fn ([], []) advertised in
+      let refs = List.rev refs and peeled = List.rev peeled in
+      let* head = head_of_refs refs head_symref in
+      Protocol.return
+        (V1 { refs = { refs; peeled; head; head_symref }; capabilities })
 
 let advertisement ctx =
   let rec version ctx =
@@ -33,9 +112,13 @@ let advertisement ctx =
         match String.trim pkt with
         | "" -> Protocol.return (List.rev acc)
         | capability -> capabilities (capability :: acc) ctx in
-      capabilities [] ctx
+      let* capabilities = capabilities [] ctx in
+      Protocol.return (V2 { capabilities })
+  | [ "version"; ("0" | "1") ] ->
+      let* pkt = Protocol.decode_pkt ctx in
+      advertisement_v1 (String.trim pkt) ctx
   | [ "version"; v ] -> Protocol.error (`Invalid_version v)
-  | _ -> Protocol.error `Invalid_pkt_line
+  | _ -> advertisement_v1 pkt ctx
 
 (* NOTE(dinosaure): we need to split [ls_refs] and [fetch] for HTTP. *)
 
@@ -50,15 +133,6 @@ let ls_refs ctx =
   let* () = Protocol.encode_pkt ctx "ref-prefix refs/heads/" in
   let* () = Protocol.encode_pkt ctx "ref-prefix refs/tags/" in
   let* () = Protocol.encode_flush_pkt ctx in
-  let attribute ~prefix attrs =
-    let fn attr =
-      if String.starts_with ~prefix attr
-      then
-        let off = String.length prefix
-        and len = String.length attr - String.length prefix in
-        Some (String.sub attr off len)
-      else None in
-    List.find_map fn attrs in
   let rec go acc peeled head_symref ctx =
     let* pkt = Protocol.decode_pkt ctx in
     match String.trim pkt with
@@ -81,24 +155,25 @@ let ls_refs ctx =
         | _ -> Protocol.error `Invalid_pkt_line
         end in
   let* refs, peeled, head_symref = go [] [] None ctx in
-  let* head =
-    match (List.assoc_opt "HEAD" refs, head_symref) with
-    | Some head, _ when head <> "unborn" ->
-        let head = Ohex.decode head in
-        let head = Carton.Uid.unsafe_of_string head in
-        Protocol.return head
-    | _, Some symref ->
-        begin match List.assoc_opt symref refs with
-        | Some head ->
-            let head = Ohex.decode head in
-            let head = Carton.Uid.unsafe_of_string head in
-            Protocol.return head
-        | None -> Protocol.error `No_branch
-        end
-    | _ -> Protocol.error `No_branch in
+  let* head = head_of_refs refs head_symref in
   Protocol.return { refs; peeled; head; head_symref }
 
-let fetch ~(want : Carton.Uid.t) q ctx =
+let rec side_band errored q ctx =
+  let* pkt = Protocol.decode_pkt ctx in
+  if String.length pkt = 0
+  then Protocol.return errored
+  else
+    let data = String.sub pkt 1 (String.length pkt - 1) in
+    match pkt.[0] with
+    | '\001' ->
+        Flux.Bqueue.put q data ;
+        side_band errored q ctx
+    | '\003' ->
+        Log.err (fun m -> m "[remote]: %s" data) ;
+        side_band true q ctx
+    | _ -> side_band errored q ctx
+
+let fetch_v2 ~(want : Carton.Uid.t) q ctx =
   let* () = Protocol.encode_pkt ctx "command=fetch" in
   let* () = Protocol.encode_pkt ctx "object-format=sha1" in
   let* () = Protocol.encode_delim_pkt ctx in
@@ -114,18 +189,39 @@ let fetch ~(want : Carton.Uid.t) q ctx =
     | pkt ->
         Log.err (fun m -> m "Unexpected section: %S" pkt) ;
         Protocol.error `Invalid_pkt_line in
-  let rec go errored ctx =
-    let* pkt = Protocol.decode_pkt ctx in
-    if String.length pkt = 0
-    then Protocol.return errored
-    else
-      let data = String.sub pkt 1 (String.length pkt - 1) in
-      match pkt.[0] with
-      | '\001' ->
-          Flux.Bqueue.put q data ;
-          go errored ctx
-      | '\003' ->
-          Log.err (fun m -> m "[remote]: %s" data) ;
-          go true ctx
-      | _ -> go errored ctx in
-  go false ctx
+  side_band false q ctx
+
+let capabilities_v1 capabilities =
+  let side_band =
+    if List.mem "side-band-64k" capabilities
+    then Some "side-band-64k"
+    else if List.mem "side-band" capabilities
+    then Some "side-band"
+    else None in
+  let advertised capability = List.mem capability capabilities in
+  let fn side_band =
+    side_band :: List.filter advertised [ "ofs-delta"; "no-progress" ] in
+  Option.map fn side_band
+
+let fetch_v1 ~capabilities ~(want : Carton.Uid.t) q ctx =
+  match capabilities_v1 capabilities with
+  | None -> Protocol.error `No_side_band
+  | Some caps ->
+      let* () =
+        Protocol.encode_pkt ctx "want %s %s\n"
+          (Ohex.encode (want :> string))
+          (String.concat " " caps) in
+      let* () = Protocol.encode_flush_pkt ctx in
+      let* () = Protocol.encode_pkt ctx "done\n" in
+      let* () =
+        let* pkt = Protocol.decode_pkt ctx in
+        match String.trim pkt with
+        | "NAK" -> Protocol.return ()
+        | pkt ->
+            begin match err_of_pkt pkt with
+            | Some msg -> Protocol.error (`Err msg)
+            | None ->
+                Log.err (fun m -> m "Unexpected acknowledgement: %S" pkt) ;
+                Protocol.error `Invalid_pkt_line
+            end in
+      side_band false q ctx
